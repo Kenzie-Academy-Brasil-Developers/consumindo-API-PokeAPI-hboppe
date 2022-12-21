b13917fe41f0055e5740fd4276c9f74a950e8d86
hboppe <hanna.nascimento31@gmail.com>
2022-12-21 12:08:31 -0500

    Consumed API to render the pokemons cards

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,3 +4,21 @@
     <h2>Magikarp</h2>
 </li>
 */
+
+function renderAllPokemons(pokemonsArray){
+
+    const pokemonList = document.querySelector('.pokemonsList__container');
+
+    const pokemons = pokemonsArray.forEach(({name, url}) => {
+        pokemonList.insertAdjacentHTML('beforeend', 
+        `
+        <li data-id=${url.slice(34, -1)}>
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${url.slice(34, -1)}.png' alt="Pokemon ${name}">
+            <h2>${name}</h2>
+        </li>
+        
+        `)
+    })
+
+    return pokemons;
+}
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,1 +1,18 @@
-/* Monte a lógica das requisições aqui */
+async function getAllPokemons() {
+
+    const loading = document.querySelector('#loading');
+    
+    const pokemons = await fetch('https://pokeapi.co/api/v2/pokemon')
+    .then((resp) => resp.json())
+    .then((resp) => {
+        renderAllPokemons(resp.results)
+    })
+
+    loading.classList.add('hidden')
+    
+    return pokemons;
+}
+
+getAllPokemons()
+
+

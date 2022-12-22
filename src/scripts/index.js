15fecc42accefb62fad17e5b7050e765bac22e66
hboppe <hanna.nascimento31@gmail.com>
2022-12-21 20:08:50 -0500

    input, button and enter key events functions were added

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,6 @@
-/* 
-<li data-id="1">
-    <img src="/src/assets/1 2.png" alt="">
-    <h2>Magikarp</h2>
-</li>
-*/
+import {getAllPokemons, getPokemonByName} from './requests.js'
 
-function renderAllPokemons(pokemonsArray){
+export function renderAllPokemons(pokemonsArray){
 
     const pokemonList = document.querySelector('.pokemonsList__container');
 
@@ -22,3 +17,64 @@ function renderAllPokemons(pokemonsArray){
 
     return pokemons;
 }
+
+export function searchPokemon(){
+    const searchButton = document.querySelector('.nav__container > button');
+    const input = document.querySelector('.nav__container > input');
+
+    const pokemonName = searchButton.addEventListener('click', (event) => {
+
+        return getPokemonByName(input.value)
+       
+    });
+
+    input.addEventListener('keydown', (event) => {
+        if(event.key === 'Enter'){
+            return getPokemonByName(input.value);
+        }
+
+    })
+
+}
+
+
+
+export function renderPokemonSearched({name, url}){
+
+    cleanPokemonList();
+    console.log({name, url})
+
+    const pokemonList = document.querySelector('.pokemonsList__container');
+
+    console.log(name, url)
+
+    pokemonList.insertAdjacentHTML('beforeend', `
+        <li data-id=${url.slice(38, -1)}>
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${url.slice(38, -1)}.png' alt="Pokemon ${name}">
+            <h2>${name}</h2>
+        </li>
+    
+    `)
+
+    return pokemonList;
+}
+
+function cleanInput(){
+    const input = document.querySelector('.nav__container > input');
+
+    input.addEventListener('input', (event) => {
+        if(event.target.value ==''){
+            return getAllPokemons();
+        }
+    })
+
+}
+
+export function cleanPokemonList(){
+    const pokemonList = document.querySelector('.pokemonsList__container');
+
+    pokemonList.innerHTML = '';
+}
+
+searchPokemon()
+cleanInput()
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,7 +1,12 @@
-async function getAllPokemons() {
+import {renderAllPokemons, searchPokemon, renderPokemonSearched, cleanPokemonList} from './index.js'
+
+export async function getAllPokemons() {
+
+    cleanPokemonList();
 
     const loading = document.querySelector('#loading');
-    
+    loading.classList.remove('hidden');
+
     const pokemons = await fetch('https://pokeapi.co/api/v2/pokemon')
     .then((resp) => resp.json())
     .then((resp) => {
@@ -9,9 +14,27 @@ async function getAllPokemons() {
     })
 
     loading.classList.add('hidden')
-    
+
     return pokemons;
 }
+
+
+export async function getPokemonByName(pokemonName){
+    const loading = document.querySelector('#loading');
+    loading.classList.remove('hidden');
+
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`)
+    .then((res) => res.json())
+    .then((res) => {
+        loading.classList.add('hidden');
+        console.log(res.forms)
+        renderPokemonSearched(res.forms[0])
+        return res;
+    })
+    .catch((err) => console.log(`er2ro ${err}`))
+
+    return pokemon;
+};
 
 getAllPokemons()
 
